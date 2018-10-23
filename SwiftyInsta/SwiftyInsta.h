232201c89c22c1e93c655b7754ab17843ad050e4
TheM4HD1 <mahdimakhdomi@outlook.com>
2018-10-24 02:21:58 +0330

    Initial Commit

---
+++ SwiftyInsta/SwiftyInsta.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyInsta.h
+//  SwiftyInsta
+//
+//  Created by Mahdi on 10/24/18.
+//  Copyright © 2018 Mahdi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyInsta.
+FOUNDATION_EXPORT double SwiftyInstaVersionNumber;
+
+//! Project version string for SwiftyInsta.
+FOUNDATION_EXPORT const unsigned char SwiftyInstaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyInsta/PublicHeader.h>
+
+
