89336f2ef2ab1d232e9378fd5deb04d8834fa575
TheM4HD1 <mahdimakhdomi@outlook.com>
2018-10-24 21:57:10 +0330

    Added 'test' for 'calculating hash'

--- SwiftyInstaTests/SwiftyInstaTests.swift
+++ SwiftyInstaTests/SwiftyInstaTests.swift
@@ -11,24 +11,11 @@ import XCTest
 
 class SwiftyInstaTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testCalculateSignatureHash() {
+        let message =
+        """
+        {"phone_id":"28484284-e646-4a29-88fc-76c2666d5ab3","username":"testusernameinstaminer","guid":"7f585e77-becf-4137-bf1f-84ab72e35eb4","device_id":"android-271e73ff77f246e7","password":"testpasswordinstaminer","login_attempt_count":"0"}
+        """
+        XCTAssertEqual("ebde2e05c55f3d41d89201ad75669e493a27408f46ef7f2395bb357456d6db5b", message.hmac(algorithm: .SHA256, key: Headers.HeaderIGSignatureValue))
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
