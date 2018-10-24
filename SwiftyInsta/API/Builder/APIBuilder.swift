89d43d2584db95c5cd2c23277266f8a1914b2ca8
TheM4HD1 <mahdimakhdomi@outlook.com>
2018-10-25 02:23:01 +0330

    Added a builder for API.

--- SwiftyInsta/API/Builder/APIBuilder.swift
+++ SwiftyInsta/API/Builder/APIBuilder.swift
@@ -7,3 +7,91 @@
 //
 
 import Foundation
+
+protocol APIBuilderProtocol {
+    func setUser(user: SessionStorage) -> APIBuilderProtocol
+    func setHttpHandler(config: URLSessionConfiguration) -> APIBuilderProtocol
+    func setRequestDelay(delay: DelayModel) -> APIBuilderProtocol
+    func build() throws -> APIHandlerProtocol
+}
+
+class APIBuilder: APIBuilderProtocol {
+    
+    private var _delay: DelayModel?
+    private var _user: SessionStorage?
+    private var _device: AndroidDeviceModel?
+    private var _request: RequestMessageModel?
+    private var _config: URLSessionConfiguration?
+    
+    func createBuilder() -> APIBuilderProtocol {
+        return APIBuilder()
+    }
+    
+    func setUser(user: SessionStorage) -> APIBuilderProtocol {
+        _user = user
+        return self
+    }
+    
+    func setHttpHandler(config: URLSessionConfiguration) -> APIBuilderProtocol {
+        _config = config
+        return self
+    }
+    
+    func setRequestDelay(delay: DelayModel) -> APIBuilderProtocol {
+        _delay = delay
+        return self
+    }
+    
+    func build() throws -> APIHandlerProtocol {
+        guard let user = _user else {
+            throw CustomErrors.runTimeError("User auth data must be specified.")
+        }
+        
+        if _request == nil {
+            _device = AndroidDeviceGenerator.getRandomAndroidDevice()
+            guard let device = _device else {
+                throw CustomErrors.runTimeError("Fail to generate random android device.")
+            }
+            
+            _request = RequestMessageModel.init(
+                phoneId: device.phoneGuid.uuidString,
+                username: user.username,
+                guid: device.deviceGuid,
+                deviceId: RequestMessageModel.generateDeviceId(),
+                password: user.password,
+                loginAttemptCount: "0"
+            )
+        }
+        
+        guard let request = _request else {
+            throw CustomErrors.runTimeError("Fail to unwrap request model.")
+        }
+        
+        if request.password.isEmpty {
+            _request?.password = user.password
+        }
+        
+        if request.username.isEmpty {
+            _request?.username = user.username
+        }
+        
+        if _device == nil && !(request.deviceId.isEmpty) {
+            _device = try? AndroidDeviceGenerator.getById(deviceId: request.deviceId)
+        }
+        
+        if _device == nil {
+            _device = AndroidDeviceGenerator.getRandomAndroidDevice()
+        }
+        
+        if _delay == nil {
+            _delay = DelayModel.init(min: 5, max: 5)
+        }
+        
+        if _config == nil {
+            _config = URLSessionConfiguration.default
+        }
+        
+        // We can safely unwrap values.
+        return APIHandler(request: _request!, user: user, device: _device!, delay: _delay!, config: _config!)
+    }
+}
