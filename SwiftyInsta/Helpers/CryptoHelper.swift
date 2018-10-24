094120276e4abf396836b4627eded7475c006c59
TheM4HD1 <mahdimakhdomi@outlook.com>
2018-10-25 02:17:49 +0330

    Added 'MD5' support to 'CryptoHelper'

--- SwiftyInsta/Helpers/CryptoHelper.swift
+++ SwiftyInsta/Helpers/CryptoHelper.swift
@@ -66,3 +66,20 @@ extension String {
     }
     
 }
+
+extension String{
+    var MD5:String {
+        get{
+            let messageData = self.data(using:.utf8)!
+            var digestData = Data(count: Int(CC_MD5_DIGEST_LENGTH))
+            
+            _ = digestData.withUnsafeMutableBytes {digestBytes in
+                messageData.withUnsafeBytes {messageBytes in
+                    CC_MD5(messageBytes, CC_LONG(messageData.count), digestBytes)
+                }
+            }
+            
+            return digestData.map { String(format: "%02hhx", $0) }.joined()
+        }
+    }
+}
