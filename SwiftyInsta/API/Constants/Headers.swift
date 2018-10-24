d975e9c80c19ed417772fc6950bf9a09e4c5efe5
TheM4HD1 <mahdimakhdomi@outlook.com>
2018-10-24 21:52:21 +0330

    Added few HTTP Headers.

--- SwiftyInsta/API/Constants/Headers.swift
+++ SwiftyInsta/API/Constants/Headers.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+struct Headers {
+    static let HeaderAcceptLanguageKey = "Accept-Language"
+    static let HeaderAcceptLanguageValue = "en-US"
+    static let HeaderIGCapablitiesKey = "X-IG-Capabilities"
+    static let HeaderIGCapablitiesValue = "3brTBw=="
+    static let HeaderIGConnectionTypeKey = "X-IG-Connection-Type"
+    static let HeaderIGConnectionTypeValue = "WIFI"
+    static let HeaderXGoogleADID = "X-Google-AD-ID"
+    static let HeaderUserAgentKey = "User-Agent"
+    static let HeaderUserAgentValue = "Instagram 44.0.0.9.93 Android (21/5.0.2; 480dpi; 1080x1776; Sony; C6603; C6603; qcom; ru_RU; 95414346)"
+    static let HeaderIGSignatureKey = "signed_body"
+    static let HeaderIGSignatureValue = "98ff843b4c4d924311f452a965f073c7566ff680ee11d8fb7ba57264ab9fbabb"
+    static let HeaderIGSignatureVersionKey = "ig_sig_key_version"
+    static let HeaderIGSignatureVersionValue = "4"
+}
