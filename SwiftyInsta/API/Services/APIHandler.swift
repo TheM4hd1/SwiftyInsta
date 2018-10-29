7f0f18edd0cb704920e74f4dcfee5bd0b3d1d138
TheM4HD1 <mahdimakhdomi@outlook.com>
2018-10-29 21:38:52 +0330

    login() method optimized and completed.

--- SwiftyInsta/API/Services/APIHandler.swift
+++ SwiftyInsta/API/Services/APIHandler.swift
@@ -20,6 +20,8 @@ class APIHandler: APIHandlerProtocol {
     private var _request: RequestMessageModel
     private var _httpHelper: HttpHelper
     private var _queue: DispatchQueue
+    private var _twoFactor: TwoFactorLoginInfoModel?
+    private var _challenge: ChallengeModel?
     
     init(request: RequestMessageModel, user: SessionStorage, device: AndroidDeviceModel, delay: DelayModel, config: URLSessionConfiguration) {
         _delay = delay
@@ -31,53 +33,104 @@ class APIHandler: APIHandlerProtocol {
     }
     
     func login(completion: @escaping (Result<LoginResultModel>) -> ()) {
-        try? _httpHelper.sendRequest(method: .get, url: URLs.getInstagramUrl(), body: [:], header: [:]) { [weak self] (data, response, error) in
-            if let error = error {
-                let info = ResultInfo.init(error: error, message: error.localizedDescription, responseType: ResponseTypes.unkown)
-                let result = Result<LoginResultModel>.init(isSucceeded: false, info: info, value: nil)
-                completion(result)
-            } else {
-                // find CSRF token
-                let fields = response?.allHeaderFields
-                let cookies = HTTPCookie.cookies(withResponseHeaderFields: fields as! [String : String], for: (response?.url)!)
-                for cookie in cookies {
-                    if cookie.name == "csrftoken" {
-                        self?._user.csrfToken = cookie.value
-                        break
+        do {
+            try _httpHelper.sendRequest(method: .get, url: URLs.getInstagramUrl(), body: [:], header: [:]) { [weak self] (data, response, error) in
+                if let error = error {
+                    let info = ResultInfo.init(error: error, message: error.localizedDescription, responseType: ResponseTypes.unkown)
+                    let result = Result<LoginResultModel>.init(isSucceeded: false, info: info, value: .responseError)
+                    completion(result)
+                    
+                } else {
+                    // find CSRF token
+                    let fields = response?.allHeaderFields
+                    let cookies = HTTPCookie.cookies(withResponseHeaderFields: fields as! [String : String], for: (response?.url)!)
+                    for cookie in cookies {
+                        if cookie.name == "csrftoken" {
+                            self?._user.csrfToken = cookie.value
+                            break
+                        }
                     }
-                }
-                
-                // Headers
-                let headers: [String: String] = [
-                    "csrf": (self?._user.csrfToken)!,
-                    Headers.HeaderXGoogleADID: (self?._device.googleAdId?.uuidString)!
-                ]
-                
-                // Creating Post Request Body
-                let signature = "\(self!._request.generateSignature(signatureKey: Headers.HeaderIGSignatureValue)).\(self!._request.getMessageString())"
-                let body: [String: Any] = [
-                    Headers.HeaderIGSignatureKey: signature.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!,
-                    Headers.HeaderIGSignatureVersionKey: Headers.HeaderIGSignatureVersionValue
-                ]
-                
-                // Request with delay
-                let delay = self!._delay.random()
-                self?._queue.asyncAfter(deadline: .now() + delay, execute: {
-                    try? self?._httpHelper.sendRequest(method: .post, url: URLs.getLoginUrl(), body: body, header: headers, completion: { (data, response, error) in
-                        if let error = error {
-                            let info = ResultInfo.init(error: error, message: error.localizedDescription, responseType: ResponseTypes.unkown)
-                            let result = Result<LoginResultModel>.init(isSucceeded: false, info: info, value: nil)
-                            completion(result)
-                        } else {
-                            // Parse Data
-                            print(String(data: data!, encoding: String.Encoding.utf8)!)
-                            let info = ResultInfo.init(error: CustomErrors.noError, message: CustomErrors.noError.localizedDescription, responseType: ResponseTypes.ok)
-                            let result = Result<LoginResultModel>.init(isSucceeded: true, info: info, value: nil)
+                    
+                    // Headers
+                    let headers: [String: String] = [
+                        "csrf": (self?._user.csrfToken)!,
+                        Headers.HeaderXGoogleADID: (self?._device.googleAdId?.uuidString)!
+                    ]
+                    
+                    // Creating Post Request Body
+                    let signature = "\(self!._request.generateSignature(signatureKey: Headers.HeaderIGSignatureValue)).\(self!._request.getMessageString())"
+                    let body: [String: Any] = [
+                        Headers.HeaderIGSignatureKey: signature.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!,
+                        Headers.HeaderIGSignatureVersionKey: Headers.HeaderIGSignatureVersionValue
+                    ]
+                    
+                    // Request with delay
+                    let delay = self!._delay.random()
+                    self?._queue.asyncAfter(deadline: .now() + delay, execute: {
+                        do {
+                            try self?._httpHelper.sendRequest(method: .post, url: URLs.getLoginUrl(), body: body, header: headers, completion: { (data, response, error) in
+                                if let error = error {
+                                    let info = ResultInfo.init( error: error, message: error.localizedDescription, responseType: ResponseTypes.unkown)
+                                    let result = Result<LoginResultModel>.init(isSucceeded: false, info: info, value: .responseError)
+                                    completion(result)
+                                    
+                                } else {
+                                    let decoder = JSONDecoder()
+                                    decoder.keyDecodingStrategy = .convertFromSnakeCase
+                                    
+                                    if let data = data {
+                                        if response?.statusCode != 200 {
+                                            do {
+                                                let loginFailReason = try decoder.decode(LoginBaseResponseModel.self, from: data)
+                                                if loginFailReason.invalidCredentials ?? false {
+                                                    let info = ResultInfo.init(error: CustomErrors.invalidCredentials, message: loginFailReason.message ?? "Invalid Credentials.", responseType: .fail)
+                                                    let value = (loginFailReason.errorType == "bad_password" ? LoginResultModel.badPassword : LoginResultModel.invalidUser)
+                                                    let result = Result<LoginResultModel>.init(isSucceeded: false, info: info, value: value)
+                                                    completion(result)
+                                                    
+                                                } else if loginFailReason.twoFactorRequired ?? false {
+                                                    let info = ResultInfo.init(error: CustomErrors.twoFactorAuthentication, message: loginFailReason.message ?? "Two Factor Authentication is required.", responseType: .fail)
+                                                    let result = Result<LoginResultModel>.init(isSucceeded: false, info: info, value: .twoFactorRequired)
+                                                    self?._twoFactor = loginFailReason.twoFactorInfo
+                                                    completion(result)
+                                                    
+                                                } else if loginFailReason.checkpointChallengeRequired ?? false {
+                                                    let info = ResultInfo.init(error: CustomErrors.challengeRequired, message: loginFailReason.message ?? "Challenge is required.", responseType: .fail)
+                                                    let result = Result<LoginResultModel>.init(isSucceeded: false, info: info, value: .challengeRequired)
+                                                    self?._challenge = loginFailReason.challenge
+                                                    completion(result)
+                                                } else {
+                                                    let info = ResultInfo.init(error: CustomErrors.unExpected(loginFailReason.errorType ?? "unexpected error type."), message: loginFailReason.message ?? "Unexpected error.", responseType: .fail)
+                                                    let result = Result<LoginResultModel>.init(isSucceeded: false, info: info, value: .exception)
+                                                    completion(result)
+                                                }
+                                            } catch {
+                                                let info = ResultInfo.init(error: error, message: error.localizedDescription, responseType: .fail)
+                                                let result = Result<LoginResultModel>.init(isSucceeded: false, info: info, value: .exception)
+                                                completion(result)
+                                            }
+                                            
+                                        } else {
+                                            let info = ResultInfo.init(error: CustomErrors.noError, message: CustomErrors.noError.localizedDescription, responseType: ResponseTypes.ok)
+                                            let result = Result<LoginResultModel>.init(isSucceeded: true, info: info, value: .success)
+                                            completion(result)
+                                        }
+                                    }
+                                }
+                            })
+                        } catch {
+                            let info = ResultInfo.init(error: error, message: error.localizedDescription, responseType: .fail)
+                            let result = Result<LoginResultModel>.init(isSucceeded: false, info: info, value: .exception)
                             completion(result)
                         }
+                        
                     })
-                })
+                }
             }
+        } catch {
+            let info = ResultInfo.init(error: error, message: error.localizedDescription, responseType: .fail)
+            let result = Result<LoginResultModel>.init(isSucceeded: false, info: info, value: .exception)
+            completion(result)
         }
     }
     
