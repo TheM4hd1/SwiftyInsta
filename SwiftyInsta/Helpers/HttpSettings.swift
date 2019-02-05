bf491efd39d94ed12ea35d83c0925a0eca653902
TheM4HD1 <mahdimakhdomi@outlook.com>
2019-02-05 20:29:46 +0330

    Customize request headers by user #10

---
+++ SwiftyInsta/Helpers/HttpSettings.swift
@@ -0,0 +1,31 @@
+//
+//  HttpSettings.swift
+//  SwiftyInsta
+//
+//  Created by Mahdi on 2/5/19.
+//  Copyright © 2019 Mahdi. All rights reserved.
+//
+
+import Foundation
+
+public class HttpSettings {
+    
+    static let shared = HttpSettings()
+    private init() {
+        
+    }
+    
+    /// default is true
+    var useFakeUserAgent = true
+    
+    private var headers: [String: String] = [:]
+    
+    /// Any existing value for the field is replaced by the new value
+    func addValue(_ value: String, forHTTPHeaderField field: String) {
+        headers.updateValue(value, forKey: field)
+    }
+    
+    func getHeaders() -> [String: String] {
+        return headers
+    }
+}
