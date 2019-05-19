962dee927ef4682bab9fc5eac42c65549ebe11f6
TheM4HD1 <mahdimakhdomi@outlook.com>
2019-05-19 12:20:59 +0430

    added a bridge for Siwa framework #18

--- SwiftyInsta/Classes/Models/SessionCache.swift
+++ SwiftyInsta/Classes/Models/SessionCache.swift
@@ -14,4 +14,9 @@ public struct SessionCache: Codable {
     public let requestMessage: RequestMessageModel
     public let cookies: [Data]
     public let isUserAuthenticated: Bool
+    
+    /// use this function from Siwa framework.
+    public static func from(cookies: [Data]) -> SessionCache {
+        return self.init(user: SessionStorage.create(username: "username", password: "password"), device: HandlerSettings.shared.device!, requestMessage: HandlerSettings.shared.request!, cookies: cookies, isUserAuthenticated: true)
+    }
 }
