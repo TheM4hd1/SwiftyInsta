83db16bc8308e0b303b9535963f08df6988fd4e8
sbertix <stefano.bertagno@gmail.com>
2019-07-19 17:04:39 +0200

    Fixed `InstagramLoginWebView` for multi-users Allow `InstagramLoginWebView` to log in several users in a row. (iOS 11 and above only)

--- SwiftyInsta/Web/InstagramLoginWebViewDelegate.swift
+++ SwiftyInsta/Web/InstagramLoginWebViewDelegate.swift
@@ -9,49 +9,47 @@
 import UIKit
 import WebKit
 
-
+@available(iOS 11, *)
 public protocol InstagramLoginWebViewDelegate {
     func userLoggedSuccessfully()
     func webViewFinishedToLoadUser(sessionCache : SessionCache, handler: APIHandlerProtocol)
 }
 
-
-public class InstagramLoginWebView: WKCookieWebView {
-    
+@available(iOS 11, *)
+public class InstagramLoginWebView: WKWebView, WKNavigationDelegate {
     public var redirectCompleted: (() -> ())?
     public var loginDelegate : InstagramLoginWebViewDelegate?
-    
-    public  override init(frame: CGRect, configurationBlock: ((WKWebViewConfiguration) -> Void)? = nil) {
-        super.init(frame: frame, configurationBlock: configurationBlock)
-        self.wkNavigationDelegate = self
-        self.setCallbacks()
+
+    // MARK: Init
+    public convenience init(frame: CGRect) {
+        self.init(frame: frame, configuration: .init())
     }
     
-    private func setCallbacks() {
-        onDecidePolicyForNavigationResponse = { (webView, response, decide) in
-            decide(.allow)
-        }
-        
-        onUpdateCookieStorage = { webView in
-            self.fetchCookies(completion: { (cookies) in
-                var isLogged = false
-                for cookie in cookies! {
-                    if cookie.name == "ds_user_id" {
-                        isLogged = true
-                    }
-                }
-                if isLogged {
-                    for cookie in cookies! {
-                        HTTPCookieStorage.shared.setCookie(cookie)
-                    }
-                    
-                }
-            })
-        }
+    public override init(frame: CGRect, configuration: WKWebViewConfiguration) {
+        // update the process pool.
+        let copy = configuration.copy() as! WKWebViewConfiguration
+        copy.processPool = WKProcessPool()
+        // init login view.
+        super.init(frame: frame, configuration: copy)
+        navigationDelegate = self
     }
     
-    public func loadInstagramLogin(isNeedPreloadForCookieSync : Bool) {
-        self.deleteAllCookies()
+    @available(*, unavailable, message: "use `init(frame:, configuration:)` instead.")
+    public init(frame: CGRect, configurationBlock: ((WKWebViewConfiguration) -> Void)? = nil) {
+        fatalError("init(frame:, configurationBlock:) has been removed")
+    }
+    
+    public required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Log in
+    public func loadInstagramLogin() {
+        // wipe all cookies and wait to load.
+        deleteAllCookies { [weak self] in self?.requestLogIn() }
+    }
+    
+    private func requestLogIn() {
         let urlString = "https://www.instagram.com/accounts/login/"
         
         // in some iOS versions, use-agent needs to be different.
@@ -66,132 +64,72 @@ public class InstagramLoginWebView: WKCookieWebView {
             self.customUserAgent = "(Linux; Android 4.4.2; SCH-I545 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.111 Mobile Safari/537.36"
         }
         
-        if isNeedPreloadForCookieSync {
-            WKCookieWebView.preloadWithDomainForCookieSync(urlString: urlString) { [weak self] in
-                self!.load(URLRequest(url: URL(string: urlString)!))
-            }
-        } else {
-            self.load(URLRequest(url: URL(string: urlString)!))
-        }
+        self.load(URLRequest(url: URL(string: urlString)!))
     }
     
-    public func isUserLoggedIn(instagramCookies : [HTTPCookie]) {
-        var isLogged = false
-        var csrfToken = ""
-        for cookie in instagramCookies {
-            if cookie.name == "ds_user_id"
-            {
-                isLogged = true
-            }
-            if cookie.name == "csrftoken"
-            {
-                csrfToken = cookie.value
-            }
-        }
+    public func isUserLoggedIn(instagramCookies : [HTTPCookie]?) {
+        // check for cookies.
+        guard let cookies = instagramCookies else { return }
+        let filtered = cookies.filter { $0.name == "ds_user_id" || $0.name == "csrftoken" }
+        guard filtered.count == 2 else { return }
         
-        if isLogged {
-            let _urlSession = URLSession(configuration: .default)
-            let user = SessionStorage.create(username: "username", password: "password")
-            let instagramHandler = try! APIBuilder()
-                .createBuilder()
-                .setHttpHandler(urlSession: _urlSession)
-                .setRequestDelay(delay: .default)
-                .setUser(user: user)
-                .build()
+        // deal with log in.
+        let session = URLSession(configuration: .default)
+        let user = SessionStorage.create(username: "username", password: "password")
+        let handler = try! APIBuilder()
+            .createBuilder()
+            .setHttpHandler(urlSession: session)
+            .setRequestDelay(delay: .default)
+            .setUser(user: user)
+            .build()
+        
+        let sessionCache = SessionCache.init(user: HandlerSettings.shared.user!,
+                                             device: HandlerSettings.shared.device!,
+                                             requestMessage: HandlerSettings.shared.request!,
+                                             cookies: cookies.toCookieData(),
+                                             isUserAuthenticated: true)
+        try? handler.login(cache: sessionCache) { _ in }
+        // notify delegate if `!= nil`.
+        loginDelegate?.userLoggedSuccessfully()
+        // obtain current user.
+        try? handler.getCurrentUser { [weak self] in
+            guard $0.isSucceeded else { return }
+            // obtain current user.
+            let currentUser = $0.value?.user
+            let shortUserModel = UserShortModel(isVerified: currentUser?.isVerified, isPrivate: currentUser?.isPrivate, pk: currentUser?.pk, profilePicUrl: currentUser?.profilePicUrl, profilePicId: nil, username: currentUser?.username, fullName: currentUser?.fullName, name: currentUser?.fullName, address: nil, shortName: nil, lng: nil, lat: nil, externalSource: nil, facebookPlacesId: nil, city: nil, biography: currentUser?.biography)
+            
+            HandlerSettings.shared.user!.loggedInUser = shortUserModel
+            HandlerSettings.shared.user?.username = shortUserModel.username!
+            HandlerSettings.shared.user!.rankToken = "\(HandlerSettings.shared.user!.loggedInUser.pk ?? 0)_\(HandlerSettings.shared.request!.phoneId )"
+            HandlerSettings.shared.user?.csrfToken = filtered.first(where: { $0.name == "csrftoken" })!.value
             
             let sessionCache = SessionCache.init(user: HandlerSettings.shared.user!, device: HandlerSettings.shared.device!, requestMessage: HandlerSettings.shared.request!, cookies: (HTTPCookieStorage.shared.cookies?.getInstagramCookies()?.toCookieData())!, isUserAuthenticated: true)
-            try? instagramHandler.login(cache: sessionCache, completion: { (result) in
-                
-            })
-            
-            if self.loginDelegate != nil {
-                self.loginDelegate?.userLoggedSuccessfully()
-            }
-            
-            try? instagramHandler.getCurrentUser { (currentUserModel) in
-                if currentUserModel.isSucceeded {
-                    let currentUser = currentUserModel.value?.user
-                    let shortUserModel = UserShortModel(isVerified: currentUser?.isVerified, isPrivate: currentUser?.isPrivate, pk: currentUser?.pk, profilePicUrl: currentUser?.profilePicUrl, profilePicId: nil, username: currentUser?.username, fullName: currentUser?.fullName, name: currentUser?.fullName, address: nil, shortName: nil, lng: nil, lat: nil, externalSource: nil, facebookPlacesId: nil, city: nil, biography: currentUser?.biography)
-                    
-                    HandlerSettings.shared.user!.loggedInUser = shortUserModel
-                    HandlerSettings.shared.user?.username = shortUserModel.username!
-                    HandlerSettings.shared.user!.rankToken = "\(HandlerSettings.shared.user!.loggedInUser.pk ?? 0)_\(HandlerSettings.shared.request!.phoneId )"
-                    HandlerSettings.shared.user?.csrfToken = csrfToken
-                    
-                    let sessionCache = SessionCache.init(user: HandlerSettings.shared.user!, device: HandlerSettings.shared.device!, requestMessage: HandlerSettings.shared.request!, cookies: (HTTPCookieStorage.shared.cookies?.getInstagramCookies()?.toCookieData())!, isUserAuthenticated: true)
-                    
-                    if self.loginDelegate != nil {
-                        self.loginDelegate?.webViewFinishedToLoadUser(sessionCache: sessionCache, handler: instagramHandler)
-                    }
-                }
-            }
-        }
-        
-    }
-    
-    public func deleteAllCookies() {
-        
-        self.cleanEveryThing()
-        
-        fetchCookies { (cookies) in
-            for cookie in cookies! {
-                self.delete(cookie: cookie)
-            }
+            // notify delegate if `!= nil`.
+            self?.loginDelegate?.webViewFinishedToLoadUser(sessionCache: sessionCache, handler: handler)
         }
     }
-    
-    func cleanEveryThing() {
+
+    // MARK: Clean cookies
+    private func fetchCookies() {
+        configuration.websiteDataStore.httpCookieStore.getAllCookies {
+            let instagramCookies = $0.getInstagramCookies()
+            self.isUserLoggedIn(instagramCookies: instagramCookies)
+        }
+    }
+    private func deleteAllCookies(completionHandler: @escaping () -> Void) {
         HTTPCookieStorage.shared.removeCookies(since: Date.distantPast)
-        print("[WebCacheCleaner] All cookies deleted")
-        
-        WKWebsiteDataStore.default().fetchDataRecords(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes()) { records in
-            records.forEach { record in
-                WKWebsiteDataStore.default().removeData(ofTypes: record.dataTypes, for: [record], completionHandler: {})
-                print("[WebCacheCleaner] Record \(record) deleted")
-            }
-        }
+        WKWebsiteDataStore.default().removeData(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes(),
+                                                modifiedSince: .distantPast,
+                                                completionHandler: completionHandler)
     }
     
-    required public init?(coder: NSCoder) {
-        super.init(coder: coder)
-    }
-    
-    
-    public override func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
-        
-    }
-    
-    public override func webView(_ webView: WKWebView, didReceiveServerRedirectForProvisionalNavigation navigation: WKNavigation!) {
-        
-    }
-    
-    public override func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
-        
-    }
-    
-    public override func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
-        
-    }
-    
-    public override func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+    // MARK: Navigation delegate
+    public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         print("URL of Finished::::" + webView.url!.absoluteString)
         
         if webView.url!.absoluteString == "https://www.instagram.com/" {
             redirectCompleted?()
         }
-        
-        self.fetchCookies(completion: { (cookies) in
-            let instagramCookies =  cookies?.getInstagramCookies()
-            self.isUserLoggedIn(instagramCookies: instagramCookies!)
-        })
-    }
-    
-    public override func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
-        
-    }
-    
-    @available(iOS 9.0, *)
-    public override func webViewWebContentProcessDidTerminate(_ webView: WKWebView) {
-        
+        self.fetchCookies()
     }
 }
