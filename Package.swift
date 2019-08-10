3e3c3fd8d769c54d5c6df989e060d7a44ee27d08
Stefano Bertagno <stefano.bertagno@gmail.com>
2019-08-10 10:13:08 +0200

    Auto stash before merge of "sb/development" and "origin/sb/development"

--- Package.swift
+++ Package.swift
@@ -21,7 +21,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftyInsta.iOS",
-            dependencies: ["KeychainSwift", "GzipSwift", "CryptoSwift"]
+            dependencies: ["KeychainSwift", "GzipSwift", "CryptoSwift"],
+            path: "SwiftyInsta"
         ),
     ]
 )
