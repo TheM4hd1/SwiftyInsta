684098faa81f595a88b1fc7c56f2f0f6b30917c4
TheM4hd1 <mahdimakhdomi@outlook.com>
2020-12-05 13:01:12 +0330

    fixed lint

--- SwiftyInsta/API/Constants/Constants.swift
+++ SwiftyInsta/API/Constants/Constants.swift
@@ -25,7 +25,9 @@ struct Constants {
     static let xIgAppId = "X-IG-App-ID"
     static let xIgAppIdValue = "124024574287414"
     static let userAgentKey = "User-Agent"
+    // swiftlint:disable line_length
     static let userAgentValue = "Instagram 160.1.0.31.120 (\(getIdentifier()); iOS \(getOsVersion()); en_US; en-US; scale=2.00; \(getScreenSize()); 246979827) AppleWebKit/420+"
+    // swiftlint:enable line_length
     static let contentTypeKey = "Content-Type"
     static let contentTypeApplicationFormValue = "application/x-www-form-urlencoded"
     static let igSignatureKey = "signed_body"
@@ -38,22 +40,23 @@ struct Constants {
     static let countValue = "1"
     static let rankTokenKey = "rank_token"
     static let bloksVersioningId = "7b2216598d8fcf84fbda65652788cb12be5aa024c4ea5e03deeb2b81a383c9e0"
-    
+
     static func getIdentifier() -> String {
-        #if os(iOS)
+        #if os(iOS) && !((arch(i386)) || arch(x86_64))
         var systemInfo = utsname()
         uname(&systemInfo)
         let machineMirror = Mirror(reflecting: systemInfo.machine)
-        let identifier = machineMirror.children.reduce("") { identifier, element in
-            guard let value = element.value as? Int8, value != 0 else { return identifier }
-            return identifier + String(UnicodeScalar(UInt8(value)))
+        let identifier = machineMirror.children.reduce(into: "") { (identifier, element) in
+            if let value = element.value as? Int8, value != 0 {
+                identifier += String(UnicodeScalar(UInt8(value)))
+            }
         }
         return identifier
         #else
         return "iPhone9,1"
         #endif
     }
-    
+
     static func getOsVersion() -> String {
         #if os(iOS)
         return UIDevice.current.systemVersion.replacingOccurrences(of: ".", with: "_")
@@ -61,7 +64,7 @@ struct Constants {
         return "13_5"
         #endif
     }
-    
+
     static func getScreenSize(scale: CGFloat = 2.0) -> String {
         #if os(iOS)
         let width = Int(UIScreen.main.bounds.width * scale)
